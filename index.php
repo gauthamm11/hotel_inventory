72ebaf90e7d2f22880b0a52e08d3e34148f8acbe
Gautham Asok <gauthamasok.dev@gmail.com>
2019-09-03 19:35:52 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,215 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <title>Inventory Software</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+      <br>
+
+      <div class="container">
+        <h2>Inventory Software</h2>
+        <br>
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs nav-justified" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" data-toggle="tab" href="#home">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu1">Inventory</a>
+          </li>
+        </ul>
+
+        <!-- Tab panes -->
+        <div class="tab-content">
+          <div id="home" class="container tab-pane active"><br>
+            <h3>DASHBOARD</h3>
+            <p>Graphs using artificial intelligence are exectued here.</p>
+          </div>
+          <div id="menu1" class="container tab-pane fade"><br>
+            <!-- b1 -->
+            <div class="pb-4">
+              <div class="row">
+                <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Item Search">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <img src="https://image.flaticon.com/icons/svg/321/321830.svg" width="20" height="auto" class="img-fluid">
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3">
+                  <button type="button" class="btn btn-success btn-block">Add New</button>
+                </div>
+              </div>
+            </div>
+            <!-- ./b1 -->
+            <!-- inventory -->
+            <div class="row">
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Rice" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Sugar" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Pepper" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Sun+Oil" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+            </div>
+            <!-- ./inventory -->
+              <!-- inventory -->
+            <div class="row">
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Rice" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Sugar" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Pepper" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Sun+Oil" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+            </div>
+            <!-- ./inventory -->
+              <!-- inventory -->
+            <div class="row">
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Rice" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Sugar" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Pepper" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+              <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 pb-3">
+                <!-- item card -->
+                <div class="card" style="">
+                  <img class="card-img-top" src="https://via.placeholder.com/250x250?text=Sun+Oil" alt="">
+                  <div class="card-body text-center p-0">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text.</p>
+                    <a href="#" class="btn btn-info btn-block">View Details</a>
+                  </div>
+                </div>
+                <!-- ./item card -->
+              </div>
+            </div>
+            <!-- ./inventory -->
+          </div>
+        </div>
+      </div>
+
+    </body>
+    </html>
